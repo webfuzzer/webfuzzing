7b066e0b83ca0cc9b83b0e33b9e62890395b1712
me2nuk <57348147+kimminwyk@users.noreply.github.com>
2021-09-02 00:44:47 +0900

    Update

--- Crawler/request.py
+++ Crawler/request.py
@@ -2,11 +2,11 @@ from selenium.webdriver import Chrome, ChromeOptions
 from requests import get, post, head, put, sessions
 import os
 
-posix = './webdriver/'
-nt = './webdriver/'
+posix = './webdriver/chromedriver'
+nt = './webdriver/chromedriver.exe'
 
 class static:
-    def __init__(self) -> None:
+    def __init__(self, url, **args) -> None:
         '''
         statis request
         module : requests 모듈 사용
@@ -18,7 +18,60 @@ class static:
         def sess(self) -> dict: requests.sessions
         '''
 
-        pass
+        self.url = url
+        self._get = get
+        self._put = put
+        self._post = post
+        self._head = head
+        self._args = args
+        self._sess = sessions
+
+    def get(self) -> dict:
+
+        r = self._get(self.url, **self._args)
+
+        return {
+            'text':r.text,
+            'status':r.status_code,
+            'history':r.history,
+            'ok':r.ok,
+        }
+
+    def put(self) -> dict:
+
+        r = self._put(self.url, **self._args)
+
+        return {
+            'text':r.text,
+            'status':r.status_code,
+            'history':r.history,
+            'ok':r.ok,
+        }
+
+    def post(self) -> dict:
+
+        r = self._post(self.url, **self._args)
+
+        return {
+            'text':r.text,
+            'status':r.status_code,
+            'history':r.history,
+            'ok':r.ok,
+        }
+
+    def head(self) -> dict:
+
+        r = self._head(self.url, **self._args)
+
+        return {
+            'text':r.text,
+            'status':r.status_code,
+            'history':r.history,
+            'ok':r.ok,
+        }
+
+    def sess(self):
+        return self._sess()
 
 class dynamic:
     def __init__(self) -> None:
@@ -31,3 +84,13 @@ class dynamic:
         '''
         
         pass
+
+class request(static, dynamic):
+    def __init__(self, url, **args) -> None:
+        super().__init__(url, **args)
+
+    def __str__(self) -> str:
+        return '<[%s,%s]>'% (static.__name__, dynamic.__name__)
+
+r = request('https://www.google.com')
+print(r.get())
