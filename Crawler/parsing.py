c689fe028c22bf57f468480b34bd94989d7b0c94
me2nuk <57348147+kimminwyk@users.noreply.github.com>
2021-09-01 11:04:29 +0900

    Update

--- Crawler/parsing.py
+++ Crawler/parsing.py
@@ -6,17 +6,57 @@ import json
 class parse:
     def __init__(self, url, **args) -> None:
         self.url = url
-        self.args = args.setdefault('timeout',2)
-    def request(self) -> str:
-        print(self.args)
-        return get_req(self.url, self.args)
+        args.setdefault('timeout',2)
+        self.args = args
+        self.Response = get_req(self.url, **self.args)
+        self.parameter = ResponseParameter(Response)
+    
+        self.status = self.Response.status_code
+
+    @property
+    def tag(self) -> dict:
+
+        self.parameter
+
+        return {
+            'response': self.Response.text, # Response text
+        }
+
 
 class ResponseParameter:
-    def __init__(self) -> None:
-        self.loads = json.loads
-        self.dumps = json.dumps
+    def __init__(self, text) -> None:
 
-    def JsonFileWrite(self, Filename, data) -> bool:
-        return True
+        self.text = text
+        self.load = json.load
+        self.dump = json.dump
+        self.mode = 'None'
 
-print(parse('https://www.google.com').request())
+    def GetTagLink(self) -> dict:
+          
+        parser = BeautifulSoup(self.text, 'html.parser')
+        for elements in parser.find_all('a')
+
+    def JsonFileRead(self, Filename) -> (bool, str):    
+
+        self.mode = 'r'
+
+        try:
+            with open(file=filename, mode=self.mode, encoding='UTF-8') as f:
+                return (True, self.load(f))
+        except:
+            return (False, '')
+
+    def JsonFileWrite(self, Filename, data) -> (bool, str):
+
+        self.mode = 'w+'
+        self.data = data
+        self.Filename
+
+        try:
+            with open(file=filename, mode=self.mode, encoding='UTF-8') as f:
+                self.dump(data, f)
+        except:
+            return (False, '')        
+
+r = parse('https://www.google.com')
+print(r.tag)
